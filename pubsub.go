4aaa5b9fe806c09e3efa292aabe0f3dd2a7cd114
dfreire <dario.freire@gmail.com>
2014-08-23 18:57:45 +0100

    update

--- pubsub.go
+++ pubsub.go
@@ -1,15 +1,15 @@
 package pubsub
 
 import (
-    "time"
+	"time"
 )
 
 type PubSub interface {
-    Subscribe(topic string, callback Callback) (Subscription, error)
-    SubscribeSync(topic string, callback CallbackSync) (Subscription, error)
+	Subscribe(topic string, callback Callback) (Subscription, error)
+	SubscribeSync(topic string, callback CallbackSync) (Subscription, error)
 	Publish(topic string, data []byte) error
-    PublishSync(topic string, data []byte, timeout time.Duration) ([]byte, error)
-    Close()
+	PublishSync(topic string, data []byte, timeout time.Duration) ([]byte, error)
+	Close()
 }
 
 type Subscription interface {
